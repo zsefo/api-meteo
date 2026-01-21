bea6cd1d4e3380b8e1ceaf0ded737d91784088ad
zsefo <remi.moisenier@gmail.com>
2026-01-21 16:07:51 +0100

    Add POST route

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
 
-let data = { temperature: null, humidite: null };
-
-app.post("/api/send", (req, res) => {
-  data = req.body;
-  console.log("Données reçues :", data);
-  res.send("OK");
-});
+let data = {
+  temperature: null,
+  humidite: null
+};
 
 app.get("/api/data", (req, res) => {
   res.json(data);
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/api/data", (req, res) => {
+  const { temperature, humidite } = req.body;
+  data.temperature = temperature;
+  data.humidite = humidite;
+  console.log("Données reçues :", data);
+  res.json({ status: "ok", data });
+});
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Serveur météo actif sur http://localhost:${PORT}`);
 });
-
